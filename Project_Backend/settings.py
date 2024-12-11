37622eba1b37dceca40639ef5d6fab46f6ee77cc
Debojit Roy <debojit94333@gmail.com>
2024-12-12 03:43:38 +0530

    w

--- Project_Backend/settings.py
+++ Project_Backend/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     'corsheaders.middleware.CorsMiddleware',
+    'whitwenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
--- Project_Backend/wsgi.py
+++ Project_Backend/wsgi.py
@@ -15,5 +15,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Project_Backend.settings")
 
 application = get_wsgi_application()
 
-app = application
 
